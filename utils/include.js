e2866f9ca1d6dd7249a13a4a33a5c2fc6546a263
Kamilla-Parhomenko <parhomenkokamilla@gmail.com>
2026-06-28 20:06:30 +0400

    :;;:

---
+++ utils/include.js
@@ -0,0 +1,26 @@
+document.addEventListener('DOMContentLoaded', async () => {
+
+    const footers = document.querySelectorAll('#footer');
+    const headers = document.querySelectorAll('#header');
+
+    for(let footer of footers) {
+        const src = footer.getAttribute('src');
+
+        const resp = await fetch(src);
+        const html = await resp.text();
+        console.log(html);
+
+        footer.outerHTML = html;
+    }
+
+    for(let header of headers) {
+        const src = header.getAttribute('src');
+
+        const resp = await fetch(src);
+        const html = await resp.text();
+
+        header.outerHTML = html;
+    }
+
+    console.log(footers);
+});
